1ba63ea71427b1fd049536b8ddf830dc786d2753
thecaffeinedev <prabhat1015@gmail.com>
2020-07-20 21:01:34 +0530

    Removed Error, added some css & connected backend to frontend

---
+++ frontend/src/Routes.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+
+
+const Routes = ()=>{
+    return(
+        <div><h1>Hi</h1></div>
+    )
+}
+
+export default Routes
